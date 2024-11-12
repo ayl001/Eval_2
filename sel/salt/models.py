b375dd3b70503267ed2e785b0ee086c2f78f881d
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-11-12 16:04:09 +0100

    début du peuplement de models

--- sel/salt/models.py
+++ sel/salt/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
 # Create your models here.
+from typing import List
+
+class Utilisateur:
+    id: int
+    nom: str
+    prenom: str
+    email: str
+    def afficher_competences -> List[Competence]:
+
+    def indique disponibilite(c: Creneau) -> bool:
+
+class Competence
+    id: int
+    nom: str
+    def afficher(self) -> str:
+
+class Creneau
+    id: int
+    date: Date
+class echange()
+    id: int
+    competence: Competence
+    creneau: Creneau
+    def afficher_details() -> str:
+
