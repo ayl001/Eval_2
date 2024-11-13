97db237075c24718784d3910fea1b3f97d521508
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-11-13 11:56:48 +0100

    typo dans setting.py

--- sel/sel/settings.py
+++ sel/sel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'salt.apps.SaltConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
