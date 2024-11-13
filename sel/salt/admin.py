2f958a263a5ab87fd145629720a7040c0957cb1f
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-11-13 20:35:57 +0100

    admin.py + bdd

--- sel/salt/admin.py
+++ sel/salt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Utilisateur
 
 # Register your models here.
+
+admin.site.register(Utilisateur)
+admin.site.register(Creneau)
+admin.site.register(Competence)
+admin.site.register(Echange)
