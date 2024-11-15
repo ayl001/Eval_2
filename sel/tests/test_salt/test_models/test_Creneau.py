4ba9171fbdc232965d308ba0bceaff5cd51eef30
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-11-15 15:15:20 +0100

    Correction des tests

--- sel/tests/test_salt/test_models/test_Creneau.py
+++ sel/tests/test_salt/test_models/test_Creneau.py
@@ -1,16 +1,18 @@
-from sel.salt.models import Creneau
+import pytest
+from salt.models import Creneau
 # test_Creneau
 
+@pytest.mark.django_db
 def test_reserver():
 
     rv = Creneau()
-    rv.date = "25/12/2024"
+    rv.date = "2024-12-24"
     rv.disponible = False
 
     assert not rv.reserver()
 
     rv = Creneau()
-    rv.date = "31/12/2024"
+    rv.date = "2024-12-31"
     rv.disponible = True
 
     assert rv.reserver()
