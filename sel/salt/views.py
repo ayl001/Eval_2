1996fafbdce66823293c278698439f7e97cd958c
Martial Bret <martial.bret@fms-ea.com>
2024-11-19 10:10:25 +0100

    Correction du bug sur l'affichage de la page d'accueil

--- sel/salt/views.py
+++ sel/salt/views.py
@@ -5,15 +5,17 @@ from .models import Creneau, Utilisateur, Competence
 
 # Create your views here.
 
+
 def index(request):
+    output = ""
     for personne in Utilisateur.objects.all(): # On n'affiche pas les caractéristique de la personne
         creneaux = personne.creneaux.all()
         talents = personne.afficher_competences()
-        output = ""
         for cr in creneaux:
             output = output + cr.__str__()
             for comp in talents:
                 output = output + ", " + comp.__str__()
-            output = output + "<br>"
+            output += "<br>"
+
     return HttpResponse(output)
 
